0fc3a5ee81c236bf2c2b0331bd42dd3661ae97c8
Stuart Illson <sillson88@gmail.com>
2014-12-19 00:19:08 -0800

    about 2 hours in

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,10 @@
 class LandingController < ApplicationController
   def index
+
+  end
+
+  def interior
+    @title = 'Quahog Apahtments | Giggity'
+    @description = 'Almost as good as watching Futurama.'
   end
 end
